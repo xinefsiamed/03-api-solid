ee3c1468d307c7468aad23bfcb25ac497770dd8e
Leonardo Augusto <leonardoaugusto.soliveira@gmail.com>
2023-12-28 18:32:46 -0300

    :white_check_mark: Test: change to accept admin

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -2,9 +2,9 @@ import "@fastify/jwt";
 
 declare module "@fastify/jwt" {
   export interface FastifyJWT {
-    payload: {}; // payload type is used for signing and verifying
     user: {
       sub: string;
+      role: "ADMIN" | "MEMBER";
     };
   }
 }
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -4,11 +4,12 @@ import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import { create } from "./create";
 import { search } from "./search";
 import { nearby } from "./nearby";
+import { verifyUserRole } from "@/http/middlewares/verify-user-role";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
-  app.post("/gyms", create);
+  app.post("/gyms", { onRequest: [verifyUserRole("ADMIN")] }, create);
 
   app.get("/gyms/search", search);
   app.get("/gyms/nearby", nearby);
